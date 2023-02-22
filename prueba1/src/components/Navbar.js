d5a8c87bc12fcc6183a4fc3ec152e054aacf4ce2
Rodrigo Cordoba <rodrigocordoba2296@gmail.com>
2023-02-22 16:46:37 -0300

    segundo commit

---
+++ prueba1/src/components/Navbar.js
@@ -0,0 +1,18 @@
+export const Navbar = () => {
+
+    return (
+        <header>
+            <div>
+                <h1>LOGO OFICIAL</h1>
+
+                <nav> 
+                    <a href="#">Enlace 1 </a>
+                    <a href="#">Enlace 2 </a>
+                    <a href="#">Enlace 3 </a>
+
+                </nav>
+
+            </div>
+        </header>
+    )
+}
